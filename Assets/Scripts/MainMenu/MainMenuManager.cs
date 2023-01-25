0766baeeb2e90fd2c2caaa78422aa89f37c780c0
RomainSem <91747026+RomainSem@users.noreply.github.com>
2023-01-25 12:02:16 +0100

    Making main menu (not fully done)

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    #region Expose
+
+    [SerializeField] Animator _animPlayer;
+    [SerializeField] Transform _playerTransform;
+    
+    #endregion
+
+    #region Unity Lyfecycle
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+    #endregion
+
+    #region Methods
+
+
+    #endregion
+
+    #region Private & Protected
+
+    #endregion
+}
